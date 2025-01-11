c8649f62524d44b71698cfedd7b92f5416a528a5
Tanya <alia.mihailova5@yandex.ru>
2025-01-11 20:42:47 +0300

    кнопки для корзины, некоторые изменения

--- script.js
+++ script.js
@@ -65,13 +65,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const targetId = this.dataset.target;
 
         // Используем if..else для показа нужного контента
-        if (targetId === 'tab-1-content') {
-            document.querySelector('#tab-1-content').classList.add('show');
-        } else if (targetId === 'tab-2-content') {
-            document.querySelector('#tab-2-content').classList.add('show');
-        } else if (targetId === 'tab-3-content') {
-            document.querySelector('#tab-3-content').classList.add('show');
-        }
+        // if (targetId === 'tab-1-content') {
+        //     document.querySelector('#tab-1-content').classList.add('show');
+        // } else if (targetId === 'tab-2-content') {
+        //     document.querySelector('#tab-2-content').classList.add('show');
+        // } else if (targetId === 'tab-3-content') {
+        //     document.querySelector('#tab-3-content').classList.add('show');
+        // }
+        document.querySelector(`#${targetId}`).classList.add('show');
+
     }
 
     function removeBorder() {
@@ -85,123 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // При клике - обработчик событий
     tabItems.forEach(item => item.addEventListener('click', selectItem));
 });
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-// const tabItems = document.querySelectorAll('.tab-item');
-// const tabContentItems = document.querySelectorAll('.tab-content-item');
-
-// // выбор tab
-// function selectItem(e) {
-//     removeBorder();
-//     removeShow();
-//     // добавить подчерк к нынешнему tab
-//     this.classList.add('tab-border');
-
-
-// // найти элемент контента для текущего таба
-// const tabContentItem = document.querySelector(`#${this.dataset.target}`);
-// }
-// // добавить класс show
-// tabContentItems.classList.add('show');
-
-// function removeBorder() {
-//     tabItems.forEach(item => item.classList.remove('tab-border'));
-// }
-
-// function removeShow() {
-//     tabContentItems.forEach(item => item.classList.remove('show'));
-// }
-
-// // при клике - обработчик событий
-// tabItems.forEach(item => item.addEventListener('click', selectItem));
-// });
-
-// const describtions = document.querySelectorAll('.description');
-// // В функции updateSlider вызываем теперь новую функцию, которая будет выставлять класс .highlight для подписи
-// function updateSlider() {
-
-  
-//     highlightSlideDescription() 
-// }
-  
-//   // Функция обновления подписи
-//   function highlightSlideDescription() {
-//     // descriptions - это список элементов, такой же как слайды и их столько же сколько и слайдов
-//     // поэтому можем использовать индекс слайдов и для подписей
-//     const highlightedDescription = descriptions[slideIndex]
-  
-//     descriptions.forEach((desc, index) => {
-//       if (index === slideIndex) {
-//         highlightedDescription.classList.add('highlight')
-//       } else {
-//         highlightedDescription.classList.remove('highlight')
-//       }
-//     })
-    
-//   }
-
-// const element = document.querySelectorAll(".description_to_second_slider"); // исправьте здесь на правильное название класса, если нужно
-// const classes = ['.fire', '.reconstr', '.warehouse', '.travel', '.ind_mount', '.mariines', '.horse_carabiner'];
-// let currentIndex = 0; // начальный индекс
-
-// // Функция, которая будет изменять стили в зависимости от текущего индекса
-// function updateHighlight() {
-//     // Снимаем выделение с предыдущего элемента
-//     element.forEach(element => element.classList.remove('highlight'));
-    
-//     // Добавляем выделение на текущий элемент, если индекс в пределах массива
-//     if (currentIndex >= 0 && currentIndex < classes.length) {
-//         document.querySelector(`.${classes[currentIndex]}`).classList.add('highlight');
-//     }
-// }
-
-// // Обработчик события для слайдера
-// element.addEventListener('click', function() {
-//     currentIndex = slideIndex; // присваиваем значение текущего индекса слайдера
-//     updateHighlight(); // обновляем выделение
-// });
-
-
-
-// Выделяю строчку
-// document.querySelectorAll(".describtion_to_second_slider");
-
-// // const change = document.querySelectorAll(".describtion_to_second_slider");
-// // change.forEach((element, index)  => {
-// let element 
-// element.addEventListener('click', function() {
-// if (slideIndex === 0) {
-//     document.querySelector('.fire').classList.add('highlight');    
-// } else if (slideIndex === 1) {
-//     document.querySelector('.reconstr').classList.add('highlight');   
-// } else if (slideIndex === 2) {
-//     document.querySelector('.warehouse').classList.add('highlight'); 
-// } else if (slideIndex === 3) {
-//     document.querySelector('.travel').classList.add('highlight');  
-// } else if (slideIndex === 4) {
-//     document.querySelector('.ind_mount').classList.add('highlight');  
-// } else if (slideIndex === 5) {
-//     document.querySelector('.mariine').classList.add('highlight');  
-// } else if (slideIndex === 6) {
-//     document.querySelector('.horse_carabiner').classList.add('highlight');  
-// } else {change.classList.remove('highlight');}
-// })
-// })
-
-// function highlight(text) {
-//     let inputText = document.querySelectorAll(".describtion_to_second_slider");
-
-//     let innerHTML = inputText.innerHTML;
-//     let index = innerHTML.indexOf(text);
-//     if (index >= 0) { 
-//      innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
-//      inputText.innerHTML = innerHTML;
-//     }
-//   }
-
-
 
 
 let advantages = [ 
@@ -282,8 +167,6 @@ const addDataToHTML = () => {
 });
   }
 };
-
-
 
 
 blocksContainer.addEventListener('click', (event) => {
@@ -396,3 +279,12 @@ const initApp = () => {
     })
 }
 initApp();
+
+blocksContainer.addEventListener('click', (event) => {
+  let clickedElement = event.target;
+
+  if (clickedElement.classList.contains('add-trolley')) {
+    clickedElement.textContent = 'Уже в корзине';
+    clickedElement.style.backgroundColor = '#4CAF50';
+  }
+});
